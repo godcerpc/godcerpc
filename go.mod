df8e4c3a3f65aae5495bddcfbbcac6f91228c35e
Jack Turner <jack.turner@asteriskinfosec.com.au>
2020-02-18 09:00:10 +0800

    initial commit of rpc code from goWMIExec by @C-Sto

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/godcerpc/godcerpc
+
+go 1.13
---
+++ uuid/main/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"strings"
+
+	"github.com/godcerpc/godcerpc/uuid"
+)
+
+func main() {
+	f := flag.String("test", "hexhexhex", "hex")
+	flag.Parse()
+	s := strings.ReplaceAll(*f, ",", "")
+	s = strings.ReplaceAll(s, "0x", "")
+	s = strings.ReplaceAll(s, " ", "")
+	b, err := hex.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(uuid.FromBytes(b))
+}
