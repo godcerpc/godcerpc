e3cbb7065a1d4c317ea1a977447fec67fd5c0420
Jack Turner <jack.turner@asteriskinfosec.com.au>
2020-02-20 15:17:15 +0800

    can now encode pointers to utf-16 strings

--- ndr64/pointer.go
+++ ndr64/pointer.go
@@ -8,10 +8,16 @@ import (
 func EncodeUniqPtr(refId uint64, referent interface{}) (out []byte) {
 	// TODO: Handle subsequent instances of the same pointer (i.e. aliasing)
 	// TODO: Handle embedded pointers
+	// FIXME: Ok this is a bad way of doing it. Yolo though for now
+
 	switch referent.(type) {
 	case uint8, uint16, uint32, uint64:
 		out = append(out, EncodeUint(refId)...)
 		out = append(out, EncodeUint(referent)...)
+	// TODO: Probably refactor this so strings don't have to be wchar_t
+	case string:
+		out = append(out, EncodeUint(refId)...)
+		out = append(out, EncodeWcharString(referent.(string))...)
 	default:
 		panic(fmt.Errorf("Type not supported for encoding: %T", referent))
 	}
--- ndr64/pointer_test.go
+++ ndr64/pointer_test.go
@@ -2,14 +2,26 @@ package ndr64
 
 import (
 	"bytes"
+	"fmt"
 
 	"testing"
 )
 
 func TestEncodeUniqPtr(t *testing.T) {
-	wantBytes := []byte{1, 0, 0, 0, 0, 0, 0, 0, 103, 69, 35, 1}
-	gotBytes := EncodeUniqPtr(uint64(1), uint32(0x01234567))
-	if bytes.Compare(wantBytes, gotBytes) != 0 {
-		t.Fail()
+	testCases := []struct {
+		refId    uint64
+		referent interface{}
+		want     []byte
+	}{
+		{1, uint32(0x01234567), []byte{1, 0, 0, 0, 0, 0, 0, 0, 103, 69, 35, 1}},
+		{2, "localhost", []byte{2, 0, 0, 0, 0, 0, 0, 0, 0x0A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x0A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x6C, 0x00, 0x6F, 0x00, 0x63, 0x00, 0x61, 0x00, 0x6C, 0x00, 0x68, 0x00, 0x6F, 0x00, 0x73, 0x00, 0x74, 0x00, 0x00, 0x00}},
+	}
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%T:%v,refId:%v", tc.referent, tc.referent, tc.refId), func(t *testing.T) {
+			gotBytes := EncodeUniqPtr(tc.refId, tc.referent)
+			if bytes.Compare(tc.want, gotBytes) != 0 {
+				t.Fail()
+			}
+		})
 	}
 }
